d924fbc3785bb655d643b6d6bf9d6e917f74cbcb
reuben21 <reuben211999@icloud.com>
2020-08-24 23:49:28 +0530

    Updated Code

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,30 +1,30 @@
 import React from "react";
 import {makeStyles, withStyles} from "@material-ui/core/styles";
-// import TextField from "@material-ui/core/TextField";
+import TextField from "@material-ui/core/TextField";
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-// const CssTextField = withStyles({
-//     root: {
-//         '& label.Mui-focused': {
-//             color: '#81b214',
-//         },
-//         '& .MuiInput-underline:after': {
-//             borderBottomColor: '#81b214',
-//         },
-//         '& .MuiOutlinedInput-root': {
-//             '& fieldset': {
-//                 borderColor: '#81b214',
-//             },
-//             '&:hover fieldset': {
-//                 borderColor: '#81b214',
-//             },
-//             '&.Mui-focused fieldset': {
-//                 borderColor: '#81b214',
-//             },
-//         },
-//     },
-// })(TextField);
+const CssTextField = withStyles({
+    root: {
+        '& label.Mui-focused': {
+            color: '#81b214',
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: '#81b214',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: '#81b214',
+            },
+            '&:hover fieldset': {
+                borderColor: '#81b214',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: '#81b214',
+            },
+        },
+    },
+})(TextField);
 
 const ColorButton = withStyles((theme) => ({
     root: {
@@ -68,6 +68,7 @@ const EventsPage = () => {
     // getModalStyle is not a pure function, we roll the style only on the first render
     const [modalStyle] = React.useState(getModalStyle);
     const [open, setOpen] = React.useState(false);
+    const [title,setTitle] = React.useState('')
     // const {classes} = this.props;
 
     const handleOpen = () => {
@@ -77,10 +78,27 @@ const EventsPage = () => {
     const handleClose = () => {
         setOpen(false);
     };
-
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        alert(`Submitting Name ${name}`)
+    }
     const body = (
         <div style={modalStyle} className={classes.paper}>
-
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <CssTextField style={{marginTop: "10px", display: "block", color: "green"}}
+                                  className={classes.margin}
+                                  label="Email-ID"
+                                  variant="outlined"
+                                  id="custom-css-outlined-input"
+                                  onChange={e=>setTitle(e.target.value)}
+                    />
+                </div>
+                <ColorButton type="submit" variant="contained"
+                             color="primary" onClick={handleOpen}>
+                    Create Event
+                </ColorButton>
+            </form>
         </div>
     );
 
